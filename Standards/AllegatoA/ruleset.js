392f35f259b77892a9ee2dd09e7c3d60b40537cf
tassoman <tassoman@gmail.com>
2019-01-29 11:50:28 +0100

    Adding Allegato A standard

---
+++ Standards/AllegatoA/ruleset.js
@@ -0,0 +1,60 @@
+_global.HTMLCS_ALLEGATOA = {
+    name: 'AllegatoA',
+    description: 'Criteri e metodi per la verifica tecnica e requisiti tecnici di accessibilità previsti dalla legge 9 gennaio 2004, n.4',
+    sniffs: [
+
+    ],
+
+    getMsgInfo: function(code) {
+        var principles = {
+            'Principle1': {
+                name: 'Perceivable',
+                link: 'http://www.w3.org/TR/WCAG20/#perceivable'
+            },
+            'Principle2': {
+                name: 'Operable',
+                link: 'http://www.w3.org/TR/WCAG20/#operable'
+            },
+            'Principle3': {
+                name: 'Understandable',
+                link: 'http://www.w3.org/TR/WCAG20/#understandable'
+            },
+            'Principle4': {
+                name: 'Robust',
+                link: 'http://www.w3.org/TR/WCAG20/#robust'
+            },
+            'Principle5': {
+                name: 'Robust',
+                link: 'http://www.w3.org/TR/WCAG20/#robust'
+            },
+            'Principle6': {
+                name: 'Robust',
+                link: 'http://www.w3.org/TR/WCAG20/#robust'
+            },
+            'Principle7': {
+                name: 'Robust',
+                link: 'http://www.w3.org/TR/WCAG20/#robust'
+            },
+            'Principle8': {
+                name: 'Robust',
+                link: 'http://www.w3.org/TR/WCAG20/#robust'
+            },
+            'Principle9': {
+                name: 'Robust',
+                link: 'http://www.w3.org/TR/WCAG20/#robust'
+            },
+            'Principle10': {
+                name: 'Robust',
+                link: 'http://www.w3.org/TR/WCAG20/#robust'
+            },
+            'Principle11': {
+                name: 'Robust',
+                link: 'http://www.w3.org/TR/WCAG20/#robust'
+            },
+            'Principle12': {
+                name: 'Robust',
+                link: 'http://www.w3.org/TR/WCAG20/#robust'
+            },
+        };
+    },
+}
